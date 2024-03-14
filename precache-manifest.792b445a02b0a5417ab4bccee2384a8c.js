904d79f585ecd159cdb25d447e6b2f6826f81946
lexeach <toaddenterprises@gmail.com>
2024-03-14 17:34:28 +0530

    c

---
+++ precache-manifest.792b445a02b0a5417ab4bccee2384a8c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "30d10e279cc7748ed91b",
+    "url": "/static/css/main.14372502.chunk.css"
+  },
+  {
+    "revision": "30d10e279cc7748ed91b",
+    "url": "/static/js/main.30d10e27.chunk.js"
+  },
+  {
+    "revision": "a86423d1c446a774f17d",
+    "url": "/static/css/1.3ab09334.chunk.css"
+  },
+  {
+    "revision": "a86423d1c446a774f17d",
+    "url": "/static/js/1.a86423d1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6ee59968e20f616e5375c1516ac90399",
+    "url": "/static/media/token-logo.6ee59968.png"
+  },
+  {
+    "revision": "9c15c893ab62bdc5c9e7a3b3fce42682",
+    "url": "/static/media/bg.9c15c893.jpg"
+  },
+  {
+    "revision": "06eef3d9797fc7bea773c595a3bddae3",
+    "url": "/static/media/bg.06eef3d9.png"
+  },
+  {
+    "revision": "ebbc23a4a884df24b652b16fee85c991",
+    "url": "/static/media/banner.ebbc23a4.jpg"
+  },
+  {
+    "revision": "0af9786668b6a51308b5196481a6f10f",
+    "url": "/static/media/Log.0af97866.png"
+  },
+  {
+    "revision": "f65086315eb94622b976f6be9337fc17",
+    "url": "/index.html"
+  }
+];
